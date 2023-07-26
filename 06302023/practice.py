814c897915234a14b3833dc746687e620f0704f0
oddvlex <103486289+oddvlex@users.noreply.github.com>
2023-07-25 18:47:34 -0700

    prac

--- 06302023/practice.py
+++ 06302023/practice.py
@@ -1,4 +1,10 @@
 print("hello world")
 birth_year = input('Birth year: ')
+    ## input() takes in a user's input, they can type this in the terminal for now 
 age = 2023 - int(birth_year)
+## int() converts a string into an integer
+## other type conversions: 
+    ## bool()
+    ## float()
+## type() can be used to check what class something is i.e., str, int, etc. 
 print(age)
