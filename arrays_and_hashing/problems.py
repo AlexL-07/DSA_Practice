26695635156fb2413f0840d1904c5a773b9c18fe
oddvlex <103486289+oddvlex@users.noreply.github.com>
2023-08-21 19:02:24 -0700

    prac

--- arrays_and_hashing/problems.py
+++ arrays_and_hashing/problems.py
@@ -780,19 +780,19 @@ class Solution:
             return (self.is_row_valid(board) and
                 self.is_col_valid(board) and
                 self.is_square_valid(board))
-    
+
         def is_row_valid(self, board):
             for row in board:
                 if not self.is_unit_valid(row):
                     return False
             return True
-    
+
         def is_col_valid(self, board):
             for col in zip(*board):
                 if not self.is_unit_valid(col):
                     return False
             return True
-    
+
         def is_square_valid(self, board):
             for i in (0, 3, 6):
                 for j in (0, 3, 6):
@@ -800,7 +800,32 @@ class Solution:
                     if not self.is_unit_valid(square):
                         return False
             return True
-    
+
         def is_unit_valid(self, unit):
             unit = [i for i in unit if i != '.']
             return len(set(unit)) == len(unit)
+
+
+# 128. Longest Consecutive Sequence
+
+class Solution(object):
+    def longestConsecutive(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+
+        nums = set(nums)
+        maxLen = 0
+
+        while nums:
+            first = last = nums.pop()
+            while first - 1 in nums:
+                first -= 1
+                nums.remove(first)
+            while last + 1 in nums: 
+                last += 1
+                nums.remove(last)
+            maxLen = max(maxLen, last - first + 1)
+        
+        return maxLen 
