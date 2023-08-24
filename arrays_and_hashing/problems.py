9c3c04323efb21c153ef1d0bbcbb40ff4e53292f
oddvlex <103486289+oddvlex@users.noreply.github.com>
2023-08-23 17:42:59 -0700

    prac

--- arrays_and_hashing/problems.py
+++ arrays_and_hashing/problems.py
@@ -829,3 +829,37 @@ class Solution(object):
             maxLen = max(maxLen, last - first + 1)
         
         return maxLen 
+
+
+# 75. Sort Colors
+
+class Solution(object):
+    def sortColors(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        red = 0
+        white = 0 
+        blue = len(nums) - 1
+
+        while white <= blue:
+            if nums[white] == 0:
+                nums[red], nums[white] = nums[white], nums[red]
+                white += 1
+                red += 1
+            elif nums[white] == 1:
+                white += 1
+            else:
+                nums[white], nums[blue] = nums[blue], nums[white]
+                blue -= 1
+            
+        return nums
+
+    # This is a dutch partitioning problem. We are classifying the array into four groups: red, white, unclassified, and blue. 
+    # Initially we group all elements into unclassified. 
+    # We iterate from the beginning as long as the white pointer is less than the blue pointer.
+
+    # If the white pointer is red (nums[white] == 0), we swap with the red pointer and move both white and red pointer forward. 
+    # If the pointer is white (nums[white] == 1), the element is already in correct place, so we don't have to swap, just move the white pointer forward. 
+    # If the white pointer is blue, we swap with the latest unclassified element.
