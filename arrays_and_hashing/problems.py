96561c8e9d3305c25ed7c5cb82eb7efc0a73fba5
oddvlex <103486289+oddvlex@users.noreply.github.com>
2023-08-29 19:24:36 -0700

    prac

--- arrays_and_hashing/problems.py
+++ arrays_and_hashing/problems.py
@@ -924,3 +924,20 @@ class Solution(object):
                 maxSum = max(maxSum, sumMap[currSum])
         
         return len(wall) - maxSum
+    
+
+
+# 122. Best Time to Buy and Sell Stock II
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        profits = []
+
+        for i in range(len(prices) - 1):
+            if prices[i] < prices[i + 1]:
+                profits.append(prices[i + 1] - prices[i])
+        
+        return sum(profits)
