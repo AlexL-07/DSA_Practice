7ed3027bb0b784402a6874af58070ad2307460af
oddvlex <103486289+oddvlex@users.noreply.github.com>
2023-09-05 15:54:14 -0700

    prac

--- arrays_and_hashing/problems.py
+++ arrays_and_hashing/problems.py
@@ -994,3 +994,17 @@ class Solution(object):
             res += len(set(s[start_idx + 1:end_idx]))
 
         return res
+
+
+# 1963. Minimum Number of Swaps to Make the String Balanced
+class Solution:
+    def minSwaps(self, s: str) -> int:
+        count = 0
+        for i in s:
+            if i == "[":
+                count += 1  # increment only if we encounter an open bracket. 
+            else:
+                if count > 0:  #decrement only if count is positive. Else do nothing and move on. This is because for the case " ] [ [ ] " we do not need to in
+                    count -= 1
+        return (count + 1) // 2
+       
