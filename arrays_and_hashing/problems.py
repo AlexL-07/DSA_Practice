741e8880efc73aad9b4638060fcb01ffe35e4a6e
oddvlex <103486289+oddvlex@users.noreply.github.com>
2023-09-06 17:29:07 -0700

    prac

--- arrays_and_hashing/problems.py
+++ arrays_and_hashing/problems.py
@@ -1029,3 +1029,31 @@ class Solution(object):
             total += (v * (v - 1)) // 2
         
         return total
+
+
+# 2002. Maximum Product of the Length of Two Palindromic Subsequences 
+class Solution(object):
+    def maxProduct(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+
+        n = len(s)
+        arr = []
+
+        for mask in range(1, 1<<n):
+            subseq = ''
+            for i in range(n):
+                if mask & (1 << i) > 0:
+                    subseq += s[i]
+            if subseq == subseq[::-1]:
+                arr.append((mask, len(subseq)))
+        
+        result = 1
+
+        for (mask1, len1), (mask2, len2) in product(arr, arr):
+            if mask1 & mask2 == 0:
+                result = max(result, len1 * len2)
+        
+        return result
