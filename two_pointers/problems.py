fdca1b76a878b6b37095f32c5b64a36af8b2635c
oddvlex <103486289+oddvlex@users.noreply.github.com>
2023-09-25 16:43:55 -0700

    prac

--- two_pointers/problems.py
+++ two_pointers/problems.py
@@ -16,3 +16,21 @@ class Solution(object):
                 newStr += c.lower()
 
         return newStr == newStr[::-1]
+
+# 680. Valid Palindrome II
+class Solution(object):
+    def validPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        h = 0
+        t = len(s) - 1
+
+        while h < t:
+            if s[h] != s[t]:
+                return s[h:t] == s[h:t][::-1] or s[h + 1:t + 1] == s[h + 1:t + 1][::-1]
+            h = h + 1
+            t = t - 1
+        
+        return True
