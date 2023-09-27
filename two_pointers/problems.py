6991b8d48a35300d04ce35aa83805d14e7406270
oddvlex <103486289+oddvlex@users.noreply.github.com>
2023-09-26 20:40:11 -0700

    prac

--- two_pointers/problems.py
+++ two_pointers/problems.py
@@ -115,3 +115,28 @@ class Solution(object):
 
             i += 1
             j -= 1
+
+# 88. Merge Sorted Array
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+        """
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: None Do not return anything, modify nums1 in-place instead.
+        """
+
+        # should merge nums2 into nums1 
+        a = m - 1
+        b = n - 1
+        ind = m + n - 1
+
+        while b >= 0:
+            if a >= 0 and nums1[a] > nums2[b]:
+                nums1[ind] = nums1[a]
+                a -= 1
+            else:
+                nums1[ind] = nums2[b]
+                b -= 1
+            ind -= 1
