a92a36660525d0cb883ca84a1ed4d688be228d2c
oddvlex <103486289+oddvlex@users.noreply.github.com>
2023-11-02 14:40:03 -0700

    prac

--- sliding_window/problems.py
+++ sliding_window/problems.py
@@ -20,7 +20,7 @@ class Solution(object):
         
         return max_profit 
 
-# Contains Duplicate II
+# 219. Contains Duplicate II
 class Solution(object):
     def containsNearbyDuplicate(self, nums, k):
         """
@@ -37,3 +37,27 @@ class Solution(object):
             dic[v] = i
         
         return False
+    
+# 1343. Number of Sub-arrays of Size K and Average Greater than or Equal to Threshold
+class Solution(object):
+    def numOfSubarrays(self, arr, k, threshold):
+        """
+        :type arr: List[int]
+        :type k: int
+        :type threshold: int
+        :rtype: int
+        """
+        s = 0 
+        res = 0
+
+        for i in range(0, k - 1):
+            s += arr[i]
+        
+        for i in range(k - 1, len(arr)):
+            s += arr[i]
+            if s / k >= threshold:
+                res += 1
+            s -= arr[i - k + 1]
+        
+        return res
+        
