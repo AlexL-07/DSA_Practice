085071a0fdda5f5daca7f9e9b41c6c7d8da96984
oddvlex <103486289+oddvlex@users.noreply.github.com>
2023-11-27 15:15:24 -0800

    prac

--- sliding_window/problems.py
+++ sliding_window/problems.py
@@ -175,4 +175,26 @@ def maxFrequency(self, nums, k):
             j += 1
             
         return maxFreq
+
+# 904. Fruit Into Baskets
+class Solution(object):
+    def totalFruit(self, fruits):
+        """
+        :type fruits: List[int]
+        :rtype: int
+        """
+        ans = 0
+        count = collections.defaultdict(int)
+
+        l = 0
+        for r, t in enumerate(fruits):
+          count[t] += 1
+          while len(count) > 2:
+            count[fruits[l]] -= 1
+            if count[fruits[l]] == 0:
+              del count[fruits[l]]
+            l += 1
+          ans = max(ans, r - l + 1)
+
+        return ans
         
