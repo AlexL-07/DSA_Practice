d169932716eac104f5732ca87cc47e15407a3df1
oddvlex <103486289+oddvlex@users.noreply.github.com>
2024-02-23 13:59:02 -0800

    prac

--- arrays_and_hashing/problems2.py
+++ arrays_and_hashing/problems2.py
@@ -200,3 +200,26 @@ class Solution(object):
     def is_unit_valid(self, unit):
         unit = [i for i in unit if i != '.']
         return len(set(unit)) == len(unit)
+
+# 128. Longest Consecutive Sequence
+class Solution(object):
+    def longestConsecutive(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+
+        nums = set(nums)
+        max_con = 0
+
+        while nums:
+            first = last = nums.pop()
+            while first - 1 in nums:
+                first -= 1
+                nums.remove(first)
+            while last + 1 in nums:
+                last += 1
+                nums.remove(last)
+            max_con = max(max_con, last - first + 1)
+        
+        return max_con
