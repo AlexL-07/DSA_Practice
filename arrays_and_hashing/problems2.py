47cc236744e64f29fa027f18d08dce5d2add7dde
oddvlex <103486289+oddvlex@users.noreply.github.com>
2024-02-26 15:55:32 -0800

    prac

--- arrays_and_hashing/problems2.py
+++ arrays_and_hashing/problems2.py
@@ -251,3 +251,19 @@ class Solution(object):
             j += 1
         
         return i == len(s)
+
+# 14. Longest Common Prefix
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+
+        prefix = strs[0]
+
+        for s in strs:
+            while not s.startswith(prefix):
+                prefix = prefix[:-1]
+        
+        return prefix
