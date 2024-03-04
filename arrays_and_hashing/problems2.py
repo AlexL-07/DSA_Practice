40735d588bd33ae73c45ea9caf2b279e43375348
oddvlex <103486289+oddvlex@users.noreply.github.com>
2024-03-04 15:02:29 -0800

    prac

--- arrays_and_hashing/problems2.py
+++ arrays_and_hashing/problems2.py
@@ -268,4 +268,19 @@ class Solution(object):
         
         return prefix
 
-# 12345678.    
+# 27. Remove Element   
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+
+        count = 0 
+        for n in nums:
+            if n != val:
+                nums[count] = n
+                count += 1
+        
+        return count    
