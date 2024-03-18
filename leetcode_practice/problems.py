bddc5a5d31b51100b7328e100f5b01324f47ef8e
oddvlex <103486289+oddvlex@users.noreply.github.com>
2024-03-18 16:28:23 -0700

    prac

---
+++ leetcode_practice/problems.py
@@ -0,0 +1,41 @@
+# 231. Power of Two
+class Solution(object):
+    def isPowerOfTwo(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        if n == 1 or n == 2:
+            return True
+        
+        if n % 2 != 0:
+            return False
+        
+        temp = 2
+        while temp < n:
+            temp *= 2
+            if temp == n:
+                return True
+        
+        return False
+
+# 326. Power of Three
+class Solution(object):
+    def isPowerOfThree(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        if n <= 0:
+            return False
+        
+        if n == 1 or n == 3:
+            return True
+        
+        temp = 3
+        while temp < n:
+            temp *= 3
+            if temp == n:
+                return True
+        
+        return False 
