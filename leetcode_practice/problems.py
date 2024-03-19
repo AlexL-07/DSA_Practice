93e07e063119518885f9d261f943c970f275fcbe
oddvlex <103486289+oddvlex@users.noreply.github.com>
2024-03-19 16:58:12 -0700

    prac

--- leetcode_practice/problems.py
+++ leetcode_practice/problems.py
@@ -117,3 +117,20 @@ class Solution(object):
             else: 
                 right = mid - 1
         return left
+
+# 263. Ugly Number
+class Solution(object):
+    def isUgly(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        if n == 0:
+            return False
+        while n % 5 == 0:
+            n /= 5
+        while n % 3 == 0:
+            n /= 3
+        while n % 2 == 0:
+            n /= 2
+        return n == 1
