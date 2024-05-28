0c8409ca14573dc5e9da60af8d331eb0c9ab3957
oddvlex <103486289+oddvlex@users.noreply.github.com>
2024-05-28 16:33:00 -0700

    prac

--- leetcode_practice/leetcode75.py
+++ leetcode_practice/leetcode75.py
@@ -93,3 +93,21 @@ class Solution(object):
             res.append(n + extraCandies >= maxCandy)
         
         return res
+
+# 605. Can Place Flowers
+class Solution(object):
+    def canPlaceFlowers(self, flowerbed, n):
+        """
+        :type flowerbed: List[int]
+        :type n: int
+        :rtype: bool
+        """
+
+        flowerbed = [0] + flowerbed + [0]
+        
+        for i in range(1,len(flowerbed)-1):
+            if flowerbed[i] ==0 and flowerbed[i-1] !=1 and flowerbed[i+1] !=1:
+                n -= 1
+                flowerbed[i] =1
+                
+        return n <= 0
