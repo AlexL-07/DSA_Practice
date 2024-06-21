e55652b6dc6d2ed90c5993cd4a3067d9d4634e5b
oddvlex <103486289+oddvlex@users.noreply.github.com>
2024-06-21 13:08:13 -0700

    prac

--- leetcode_practice/leetcode75.py
+++ leetcode_practice/leetcode75.py
@@ -257,3 +257,21 @@ class Solution(object):
                 orig -= 1
             else:
                 i += 1
+
+# 392. Is Subsequence
+class Solution(object):
+    def isSubsequence(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        i = 0 
+        j = 0
+
+        while i < len(s) and j < len(t):
+            if s[i] == t[j]:
+                i += 1
+            j += 1
+        
+        return i == len(s)
