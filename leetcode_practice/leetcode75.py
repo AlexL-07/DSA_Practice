8a09431c46ace14bc1ab7be48933d260e1842168
oddvlex <103486289+oddvlex@users.noreply.github.com>
2024-06-25 16:07:00 -0700

    prac

--- leetcode_practice/leetcode75.py
+++ leetcode_practice/leetcode75.py
@@ -506,3 +506,14 @@ class Solution(object):
             leftSum += n
         
         return -1 
+
+# 2215. Find the Difference of Two Arrays
+class Solution(object):
+    def findDifference(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[List[int]]
+        """
+
+        return [set(nums1) - set(nums2), set(nums2) - set(nums1)]
