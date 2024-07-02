d2f2652aaa2077f6905978132f5515ee3baaab2d
oddvlex <103486289+oddvlex@users.noreply.github.com>
2024-07-02 15:15:05 -0700

    prac

--- leetcode_practice/leetcode75.py
+++ leetcode_practice/leetcode75.py
@@ -680,3 +680,30 @@ class Solution:
             curr_str = stack.pop() + curr_str
         
         return curr_str
+
+# 933. Number of Recent Calls
+from collections import deque
+class RecentCounter(object):
+
+    def __init__(self):
+        self.q = deque()
+        
+
+    def ping(self, t):
+        """
+        :type t: int
+        :rtype: int
+        """
+        self.q.append(t)
+
+        while t - self.q[0] > 3000:
+            self.q.popleft()
+        
+        return len(self.q)
+
+# Your RecentCounter object will be instantiated and called as such:
+# obj = RecentCounter()
+# param_1 = obj.ping(t)
+        
+
+
